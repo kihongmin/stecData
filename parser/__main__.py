319b9f708501a6ba49a4ac7bc1bfb9aa72466ccf
Leohn <leohnlee@gmail.com>
2020-02-29 18:09:12 +0900

    [NLP] text -> json + csv

--- parser/__main__.py
+++ parser/__main__.py
@@ -1,4 +1,6 @@
 import os
+import json
+import pandas
 from datetime import datetime
 
 from . import STECRegularizer
@@ -13,14 +15,19 @@ regularizer = STECRegularizer(
                     syns_words=dic+'syns_words.pkl',
                 )
 
-old = '../dataset/old/'
+tmp = '../dataset/tmp/'
 new = '../dataset/new/'
 new = os.path.abspath(new)
 docs = os.listdir(new)
 ret = []
 for d, doc in enumerate(docs):
     with open(new+doc, 'r') as f:
-        lines = f.readlines()
+        doc_js = json.load(f)
+    lines = doc_js.get('content')
+    URL = doc_js.get('URL')
+    if lines is None:
+        print(f'file [{doc}] in [{new}] could not readable!')
+
     lines = [line.rstrip('\n\s\t\r\v') for line in lines]
     for line in lines:
         ret += regularizer.run(line)
@@ -29,17 +36,24 @@ for d, doc in enumerate(docs):
     os.remove(new+doc)
     print(f'file [{doc}] in [{new}] is removed!')
 
-    if d%DOC_PACK != 0:
-        continue
-
-    # 파싱된 단어 문서 생성
-    now = datetime.now()
-    now = [now.year, now.month, now.day, now.hour, now.minute, now.second]
-    now = ''.join([str(n) for n in now])
-    doc = old+d.zfill(3)+'_'+now
+    doc = tmp+d.zfill(3)+'_'+now+'.csv'
     with open(doc, 'w') as f:
-        temp = '\n'.join(ret)
-        f.write(temp)
-    if d==DOC_PACK*PEOPLE:
-        break
-    ret = []
+        spamwriter = csv.writer(f, delimiter=',',
+                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        spamwriter.writerow(['URL', URL])
+        spamwriter.writerow(ret)
+
+    # if d%DOC_PACK != 0:
+    #     continue
+
+    # # 파싱된 단어 문서 생성
+    # now = datetime.now()
+    # now = [now.year, now.month, now.day, now.hour, now.minute, now.second]
+    # now = ''.join([str(n) for n in now])
+    # doc = tmp+d.zfill(3)+'_'+now
+    # with open(doc, 'w') as f:
+    #     temp = '\n'.join(ret)
+    #     f.write(temp)
+    # if d==DOC_PACK*PEOPLE:
+    #     break
+    # ret = []
