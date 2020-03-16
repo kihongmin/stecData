1210d7664cd1a9a34ec61587c020a380532353ad
kihongmin <minkh93@gmail.com>
2020-03-16 16:35:29 +0900

    sorry

--- test/test.go
+++ test/test.go
@@ -1,10 +1,6 @@
 package main
 
 import (
-<<<<<<< HEAD
-=======
-	"geekermeter-data/crawler"
->>>>>>> develop
 	"geekermeter-data/crawler/netmarble"
 )
 
@@ -12,16 +8,10 @@ func main() {
 
 	//kakao.Kakao()
 	//coupang.Coupang()
-<<<<<<< HEAD
 	//var box crawler.Job
 	//box.URL = "https://m.netmarble.com/rem/www/notice.jsp?anno_id=1583010&annotype=all"
 	netmarble.Start()
 	//nexon.Start()
-=======
-	var box crawler.Job
-	box.URL = "https://m.netmarble.com/rem/www/notice.jsp?anno_id=1583010&annotype=all"
-	netmarble.BodyText(box)
->>>>>>> develop
 	//rocketpunch.Start()
 	//crafton.Crafton()
 	//naver.Naver()
