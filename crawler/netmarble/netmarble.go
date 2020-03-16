825f28e99df5b8fc628f876f09b29516624e884e
kihongmin <minkh93@gmail.com>
2020-03-16 16:08:57 +0900

    nono

--- crawler/netmarble/netmarble.go
+++ crawler/netmarble/netmarble.go
@@ -2,6 +2,8 @@ package netmarble
 
 import (
 	"context"
+	"encoding/json"
+	"io/ioutil"
 	"log"
 	"time"
 
@@ -47,44 +49,32 @@ func Netmarble() []crawler.Job {
 		//url
 		var nodes []*cdp.Node
 		var titleNode []*cdp.Node
-		var levelNode []*cdp.Node
+		var newbieNode []*cdp.Node
 		var dateNode []*cdp.Node
-		var groupNode []*cdp.Node
 
 		err := chromedp.Run(ctx,
 			chromedp.Nodes(baseSelector+"div.cw_jopinfo > a", &nodes),
 			chromedp.Nodes(baseSelector+"div.cw_jopinfo > a > span.cw_title", &titleNode, chromedp.ByQueryAll),
-			chromedp.Nodes(baseSelector+"div.cw_jopinfo > a > span.cw_info > span.cw_type", &levelNode, chromedp.ByQueryAll),
-			chromedp.Nodes(baseSelector+"div.cw_group", &groupNode, chromedp.ByQueryAll),
+			chromedp.Nodes(baseSelector+"div.cw_jopinfo > a > span.cw_info > span.cw_type", &newbieNode, chromedp.ByQueryAll),
 			chromedp.Nodes(baseSelector+"div.cw_jopinfo > a > span.cw_info > span.cw_range", &dateNode, chromedp.ByQueryAll),
 		)
 		crawler.ErrHandler(err)
 
 		for i, row := range nodes {
 			temp[i].URL = "https://m.netmarble.com/rem/www" + row.AttributeValue("href")[1:]
+			temp[i].Origin = "Netmarble"
 		}
 		//title
 		for i, row := range titleNode {
 			temp[i].Title = row.Children[0].NodeValue
 		}
-		//level
-		/*
-			for i, row := range levelNode {
-				temp[i].level = row.Children[0].NodeValue
-			}
-		*/
-		//group
-		/*
-			for i, row := range groupNode {
-				temp[i].group = row.Children[0].NodeValue
-			}
-		*/
-		//date
-		/*
-			for i, row := range dateNode {
-				temp[i].date = row.Children[0].NodeValue
-			}
-		*/
+		for i, row := range newbieNode {
+			temp[i].Newbie = row.Children[0].NodeValue
+		}
+		for i, row := range dateNode {
+			temp[i].StartDate = row.Children[0].NodeValue[:8]
+		}
+
 		crawledData = append(crawledData, temp...)
 
 		var currPage string
@@ -113,4 +103,64 @@ func Netmarble() []crawler.Job {
 	}
 
 	return crawledData
+}
+
+func BodyText(box crawler.Job) { //현재 쓸데없는 값까지 하는 중->예외처리 실패로 인해..
+	ctx, cancel := chromedp.NewContext(context.Background())
+	defer cancel()
+
+	// run task list
+	var loc string
+	err := chromedp.Run(ctx,
+		chromedp.Navigate(box.URL),
+		chromedp.Location(&loc),
+	)
+	crawler.ErrHandler(err)
+
+	var nodes []*cdp.Node
+	var candNodes []*cdp.Node
+	var sectionName string
+	err = chromedp.Run(ctx,
+		chromedp.Nodes("body > div.main > div.position-show > div > div > div.content-body.col-item.col-xs-12.col-sm-12.col-md-12.col-lg-8>section",
+			&nodes),
+	)
+	crawler.ErrHandler(err)
+	for _, row := range candNodes {
+		next_html := row.FullXPath() + "/td[2]"
+		var temp string
+		err = chromedp.Run(ctx,
+			chromedp.Text(next_html,
+				&temp),
+		)
+		if temp == "기간" {
+			err = chromedp.Run(ctx,
+				chromedp.Text(row.FullXPath()+"/td[3]",
+					&box.StartDate),
+			)
+		}
+
+		crawler.ErrHandler(err)
+	}
+	acceptList := []string{"section-stacks", "section-position", "section-requirements", "section-preference"}
+	presentList := make([]string, 4)
+	presentNum := 0
+	for _, row := range nodes {
+		sectionName = row.AttributeValue("class")
+		_, found := Find(acceptList, sectionName)
+		if found == true {
+			presentList[presentNum] = sectionName
+			presentNum++
+		}
+	}
+	presentList = presentList[:presentNum]
+
+	box.Content = make([]string, presentNum)
+	for index, name := range presentList {
+		selurl := "body > div.main > div.position-show > div > div > div.content-body.col-item.col-xs-12.col-sm-12.col-md-12.col-lg-8 > section." + name
+		box.Content[index] = getdetail(ctx, selurl)
+	}
+
+	doc, _ := json.Marshal(box)
+	_ = ioutil.WriteFile("./dataset/test/"+crawler.Exceptspecial(box.URL)+".json", doc, 0644)
+
 }
--- test/test.go
+++ test/test.go
@@ -1,19 +1,22 @@
 package main
 
-import "geekermeter-data/crawler/programmers"
+import (
+	"geekermeter-data/crawler"
+	"geekermeter-data/crawler/netmarble"
+)
 
 func main() {
 
 	//kakao.Kakao()
 	//coupang.Coupang()
-	//var box crawler.Job
-	//box.URL = "https://career.nexon.com/user/recruit/notice/noticeView?joinCorp=NX&reNo=20170363"
-	//nexon.BodyText(box)
+	var box crawler.Job
+	box.URL = "https://m.netmarble.com/rem/www/notice.jsp?anno_id=1583010&annotype=all"
+	netmarble.BodyText(box)
 	//rocketpunch.Start()
 	//crafton.Crafton()
 	//naver.Naver()
 	//ncsoft.Ncsoft()
 	//netmarble.Netmarble()
-	programmers.Start()
+	//programmers.Start()
 
 }
