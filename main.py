60ac96c4a3bc0c00fcf9f1a4970fd4d8e9c16ef4
kihongmin <minkh93@gmail.com>
2020-05-02 15:30:34 +0900

    [Crawler]chromedriver 삭제, python main.py main --chrome_path=''으로 실행

--- main.py
+++ main.py
@@ -3,10 +3,14 @@ from crawler.netmarble import netmarble
 from crawler.programmers import programmers
 from crawler.rocketpunch import rocketpunch
 
-if __name__ == "__main__":
-    chromedriver_path = '../geekermeter-data/chromedriver'
+def main(chrome_path=None):
     json_list = []
-    json_list.extend(nexon.run(chromedriver_path))
-    json_list.extend(netmarble.run(chromedriver_path))
-    json_list.extend(rocketpunch.run(chromedriver_path))
-    json_list.extend(programmers.run(chromedriver_path))
+    json_list.extend(nexon.run(chrome_path))
+    json_list.extend(netmarble.run(chrome_path))
+    json_list.extend(rocketpunch.run(chrome_path))
+    json_list.extend(programmers.run(chrome_path))
+    return json_list
+
+main()
+
+#if __name__ == "__main__":
