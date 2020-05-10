7121b9ababe065283b7cd9eeddcc86bef863fe47
Leohn <leohnlee@gmail.com>
2020-05-10 15:07:13 +0900

    프로그래머스 리팩토링

--- crawler/programmers.py
+++ crawler/programmers.py
@@ -1,23 +1,26 @@
-import re,os,sys,json
+import re
 
-import selenium
+from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 
-from . import connect, transfrom_date, make_newbie, programmers_newbie
-from recruitment import Recruitment
-from selenium import webdriver
+from . import connect
+from ..es.recruitment import Recruitment
+from ..es.level import Level
+from ..es.start_date import StartDate
+
 
 start_url = 'https://programmers.co.kr/job'
 base_url = 'https://programmers.co.kr'
 
+
 def run():
     driver = connect()
     driver.get(start_url)
-    while True:
 
+    while True:
         WebDriverWait(driver, 10).until(
             EC.presence_of_element_located((By.ID, "tab_position"))
         )
@@ -26,14 +29,12 @@ def run():
 
         posts = soup.select('#list-positions-wrapper > ul > li > div.item-body')
         for post in posts:
-
             post_main = post.select('h4 > a')[0]
             post_url = base_url + post_main.get('href')
             post_title = post_main.text
 
             company_name = post.select('h5')[0].text
-            post_date = ''
-            post_newbie = programmers_newbie(post.select('ul.company-info > li.experience')[0].text)
+            post_level = Level.programmers2code(post.select('ul.company-info > li.experience')[0].text)
 
             tmp_driver = connect()
             tmp_driver.get(post_url)
@@ -50,16 +51,14 @@ def run():
                 title=post_title,
                 url = post_url,
                 company = company_name,
-                start_date = '',
-                level = post_newbie,
-                job='',
+                start_date = None,
+                level = post_level,
+                job=None,
                 contents=post_contents
             )
             tmp_post.run()
-            print(tmp_post.get())
 
         _next = soup.select('#paginate > nav > ul > li.next.next_page.page-item > a')[-1].get('href')
         if _next == '#':
             break
-        else:
-            driver.get('https://programmers.co.kr'+_next)
+        driver.get(base_url+_next)
