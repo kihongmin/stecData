4a6f0f8d12b998987752e4b064b565497806b216
Leohn <leohnlee@gmail.com>
2020-05-10 15:07:20 +0900

    로켓펀치 리팩토링

--- crawler/rocketpunch.py
+++ crawler/rocketpunch.py
@@ -1,20 +1,24 @@
-import re,os,sys,json
+import re
 
-import selenium
+from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 
-from . import connect, transfrom_date, make_newbie, programmers_newbie
-from recruitment import Recruitment
-from selenium import webdriver
+from . import connect
+from ..es.recruitment import Recruitment
+from ..es.level import Level
+from ..es.start_date import StartDate
+
 
 start_url = 'https://www.rocketpunch.com/jobs?page=1'
+
 
 def run():
     driver = connect()
     driver.get(start_url)
+
     while True:
         WebDriverWait(driver, 10).until(
             EC.presence_of_element_located((By.ID, "company-list"))
@@ -27,14 +31,24 @@ def run():
             company_name = company.select('div.content > div.company-name > a > h4 > strong')[0].text
             post_list = company.select('div.content > div.company-jobs-detail > div.job-detail')
             for post in post_list:
-                post_date = transfrom_date(post.select('div.job-dates > span')[-1].text,True)
-                #if not post_date:
-                #    continue
+                post_date = StartDate.transform(
+                    date=post.select('div.job-dates > span')[-1].text,
+                    source='rocketpunch')
                 post_main = post.select('div > a.nowrap.job-title.primary.link')[0]
                 post_title = post_main.text
 
                 post_url = 'https://www.rocketpunch.com' + post_main.get('href')
-                post_newbie = make_newbie(post.select('div > span.job-stat-info')[0].text.replace(',',' ').split())
+                levels = []
+                for level in post.select('div > span.job-stat-info')[0].text.replace(',',' ').split():
+                    levels.append(
+                        Level.string2code(
+                            text=level
+                        )
+                    )
+                levels = [level for level in levels if level is not None]
+                levels = sorted(levels)
+                if len(levels) < 1:
+                    levels = [Level.unlimited]
 
                 tmp_driver = connect()
                 tmp_driver.get(post_url)
@@ -51,15 +65,15 @@ def run():
                         post_contents.append(section.parent.text.replace('\n',' '))
 
                 tmp_post = Recruitment(
-                    title=post_title,
+                    title = post_title,
                     url = post_url,
                     company = company_name,
-                    start_date = '',
+                    start_date = post_date,
                     level = post_newbie,
-                    job='',
-                    contents=post_contents
+                    job = None,
+                    contents = post_contents
                 )
-                #tmp_post.run()
+                tmp_post.run()
 
         _next = driver.find_elements_by_css_selector('#search-results > div.ui.blank.right.floated.segment > div.ui.pagination.menu > a')[-1].get_attribute('href')
         if _next:
